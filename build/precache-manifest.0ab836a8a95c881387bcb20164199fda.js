2adb1863ac4c978b35868ba274b6deb1b99cf5ed
Anna <APodoynitsyna@smartspaceplc.com>
2019-06-07 10:53:31 +0300

    add build

---
+++ build/precache-manifest.0ab836a8a95c881387bcb20164199fda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c5fe98ea3dc5323084c6179a97f5c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c9d2c2ac816767a3ac4",
+    "url": "/static/css/2.7413669b.chunk.css"
+  },
+  {
+    "revision": "7c93249d954d6efb658b",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5c9d2c2ac816767a3ac4",
+    "url": "/static/js/2.1e0ee2d6.chunk.js"
+  },
+  {
+    "revision": "7c93249d954d6efb658b",
+    "url": "/static/js/main.34bce149.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
